dc51476e497e018d51fcd829b4e55588eae8ac62
Michał Jurewicz <malauch@emtb.pl>
2021-01-21 07:03:23 +0100

    fixed tests

--- Tests/ReplaceImageUrlPluginTests/ReplaceImageUrlPluginTests.swift
+++ Tests/ReplaceImageUrlPluginTests/ReplaceImageUrlPluginTests.swift
@@ -5,20 +5,20 @@ import Ink
 
 final class ReplaceImageUrlPluginTests: XCTestCase {
 	func testReplacingLocalPathForRemote_withDefultConfig() {
-		let parser = MarkdownParser(modifiers: [.replaceImageUrlPlugin()])
+		let parser = MarkdownParser(modifiers: [.replaceImageUrl()])
 		
 		let md = """
       Some text
-         
+
       ![Image](./../../Resources/images/image.png)
-      			
+
       More text
       """
 		
 		let outputHtml = parser.html(from: md)
-
+		
 		let expectedHtml = """
-		<p>Some text<br><img src="/images/image.png" alt="Image"/> More text</p>
+		<p>Some text</p><img src="/images/image.png" alt="Image"/><p>More text</p>
 		"""
 		
 		XCTAssertEqual(expectedHtml, outputHtml)
@@ -27,20 +27,20 @@ final class ReplaceImageUrlPluginTests: XCTestCase {
 	func testReplacingLocalPathForRemote_withCustomConfig() {
 		let config = ReplaceLocalPathConfig(pathRegex: #"replace\d*this"#, with: "newPath")
 		
-		let parser = MarkdownParser(modifiers: [.replaceImageUrlPlugin(config: config)])
+		let parser = MarkdownParser(modifiers: [.replaceImageUrl(config: config)])
 		
 		let md = """
       Some text
-         
+
       ![Image](/a/b/Resources/replace123this/images/image.png)
-      			
+
       More text
       """
 		
 		let outputHtml = parser.html(from: md)
 		
 		let expectedHtml = """
-		<p>Some text<br><img src="/a/b/Resources/newPath/images/image.png" alt="Image"/> More text</p>
+		<p>Some text</p><img src="/a/b/Resources/newPath/images/image.png" alt="Image"/><p>More text</p>
 		"""
 		
 		XCTAssertEqual(expectedHtml, outputHtml)
